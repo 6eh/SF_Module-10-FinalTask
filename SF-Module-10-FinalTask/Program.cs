3f5acdeca8c98f12cb1f710ac617616688a996c0
6eh <mcls@list.ru>
2023-02-27 22:46:18 +0300

    Skill Faktory Module 10 Final Task

---
+++ SF-Module-10-FinalTask/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace SF_Module_10_FinalTask
+{
+    class Program
+    {
+        static ILogger Logger { get; set; }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int a = GetNumber("Enter first number (A): ");
+                int b = GetNumber("Enter second number (B): ");
+
+                Calc calc = new Calc();
+                Console.WriteLine($"    Result: {a} + {b} = {calc.Sum(a, b)}\n");
+            }            
+        }
+
+        static int GetNumber(string message)
+        {
+            Logger = new Logger();
+            var worker = new Worker(Logger);
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(message);
+                try
+                {                    
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    worker.Work(num);
+                    return num;
+                }
+                catch (Exception ex)
+                {
+                    worker.ErrorMessage(ex);
+                }
+            }
+        }
+    }
+
+    public interface ICalc
+    {
+        public int Sum(int a, int b);       
+    }
+
+    public class Calc : ICalc
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+
+    public class Logger : ILogger
+    {
+        public void Event(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Error(string message)
+        {
+            Console.WriteLine(message);
+        }        
+    }
+
+    public interface IWorker
+    {
+        void Work(int num);
+    }
+}
+
---
+++ SF-Module-10-FinalTask/Worker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace SF_Module_10_FinalTask
+{
+	public class Worker : IWorker
+	{
+        ILogger Logger { get; }
+
+        public Worker(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work(int num)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Logger.Event($"Entered: {num}");
+        }
+
+        public void ErrorMessage(Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Logger.Error(ex.Message);
+        }
+    }
+}
+
